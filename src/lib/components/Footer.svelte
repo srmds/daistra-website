fd2c54d90f2662aa0617ae88ea0d61906e45fc90
Steven Ramdas <steven.ramdas@vattenfall.com>
2023-10-11 23:27:00 +0200

    Implement working navbar on mobile, tweak styling

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,15 +2,15 @@
 	<div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
 		<div class="md:flex md:justify-between">
 			<div class="mb-6 md:mb-0">
-				<span class="text-white font-bold text-3xl">Logo</span>
+				<span class="text-white font-bold text-3xl">Daistra | Data AI Strategy </span>
 
 				<ul class="text-gray-400 font-medium mt-3 space-y-2">
 					<li class="flex items-center gap-1">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
-							width="15"
-							height="14"
-							viewBox="0 0 15 14"
+							width="40"
+							height="40"
+							viewBox="0 0 24 24"
 							fill="none">
 							<path
 								d="M7.49998 1.16669C5.04998 1.16669 2.83331 3.04502 2.83331 5.95002C2.83331 7.80502 4.26248 9.98669 7.11498 12.5009C7.33665 12.6934 7.66915 12.6934 7.89081 12.5009C10.7375 9.98669 12.1666 7.80502 12.1666 5.95002C12.1666 3.04502 9.94998 1.16669 7.49998 1.16669ZM7.49998 7.00002C6.85831 7.00002 6.33331 6.47502 6.33331 5.83335C6.33331 5.19169 6.85831 4.66669 7.49998 4.66669C8.14165 4.66669 8.66665 5.19169 8.66665 5.83335C8.66665 6.47502 8.14165 7.00002 7.49998 7.00002Z"
@@ -21,9 +21,9 @@
 					<li class="flex items-center gap-1">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
-							width="15"
-							height="14"
-							viewBox="0 0 15 14"
+							width="40"
+							height="40"
+							viewBox="0 0 24 24"
 							fill="none">
 							<path
 								d="M12.1667 2.33334H2.83335C2.19169 2.33334 1.67252 2.85834 1.67252 3.50001L1.66669 10.5C1.66669 11.1417 2.19169 11.6667 2.83335 11.6667H12.1667C12.8084 11.6667 13.3334 11.1417 13.3334 10.5V3.50001C13.3334 2.85834 12.8084 2.33334 12.1667 2.33334ZM12.1667 4.66668L7.50002 7.58334L2.83335 4.66668V3.50001L7.50002 6.41668L12.1667 3.50001V4.66668Z"
@@ -37,13 +37,12 @@
 
 			<div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
 				<div>
-					<h2 class="mb-6 text-sm font-semibold uppercase text-white">Legal</h2>
 					<ul class=" text-gray-400 font-medium">
 						<li class="mb-4">
-							<a href="#" class="hover:underline">Privacy Policy</a>
+							<a href="https://www.kvk.nl/zoeken/handelsregister/?kvknummer=87654321" class="hover:underline">KVK: 00000000</a>
 						</li>
 						<li>
-							<a href="#" class="hover:underline">Terms &amp; Conditions</a>
+							<a href="https://ec.europa.eu/taxation_customs/vies/rest-api/ms/NL/vat/NL0087654321B99" class="hover:underline">VAT: NL00000000B00 </a>
 						</li>
 					</ul>
 				</div>
@@ -51,16 +50,16 @@
 		</div>
 		<hr class="my-6 sm:mx-auto border-gray-700 lg:my-8" />
 		<div class="sm:flex sm:items-center sm:justify-between">
-			<span class="text-sm sm:text-center text-gray-400"
-				>© 2023 <a href="#" class="hover:underline">Daistra™</a>. All Rights Reserved.
+			<span class="text-gray-400"
+				>© 2023 <a href="/" class="hover:underline">Daistra</a>. All Rights Reserved.
 			</span>
 			<div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
 				<a href="https://www.linkedin.com/in/srmds/" class="text-gray-500 hover:text-white">
 					<svg
 					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
+					class="h-20"
 					fill="currentColor"
-					viewBox="0 0 24 24">
+					viewBox="0 0 30 40">
 					<path
 						d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
 					</svg>
@@ -70,9 +69,9 @@
 				<a href="https://github.com//srmds" class="text-gray-500 hover:text-white">
 					<svg
 					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
+					class="h-20"
 					fill="currentColor"
-					viewBox="0 0 24 24">
+					viewBox="0 0 30 40">
 					<path
 					  d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
 				  </svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	<title>Daistra | Data & AI Strategy</title>
 	<meta
 		name="description"
-		content="Template with Svelte and TailwindCss"
+		content="Daistra | Data & AI Strategy"
 	/>
 </svelte:head>
 
@@ -24,8 +24,7 @@
 <section id="service" class="px-6 bg-white py-12">
 	<h2 class="font-bold text-2xl md:text-4xl lg:text-5xl text-center">Services</h2>
 	<div
-		class="grid max-w-screen-xl grid-cols-1 place-items-center px-4 py-8 mx-auto gap-8 xl:gap-0 lg:py-16 md:grid-cols-3"
-	>
+		class="grid max-w-screen-xl grid-cols-1 place-items-center px-4 py-8 mx-auto gap-8 xl:gap-0 lg:py-16 md:grid-cols-3">
 		<div class="flex flex-col space-y-2 items-start px-6 py-6">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
@@ -33,14 +32,12 @@
 				height="58"
 				viewBox="0 0 55 58"
 				fill="none"
-				class="text-indigo-500"
-			>
+				class="text-indigo-500">
 				<path
 					fill-rule="evenodd"
 					clip-rule="evenodd"
 					d="M45.2856 28.6429C45.2856 30.5749 46.8164 32.1469 48.6979 32.1469C49.6466 32.1469 50.4166 32.9376 50.4166 33.9119V40.2681C50.4166 45.6431 46.1587 50.0155 40.9246 50.0155H14.0777C8.84352 50.0155 4.58331 45.6431 4.58331 40.2681V33.9119C4.58331 32.9376 5.35331 32.1469 6.30206 32.1469C8.18581 32.1469 9.71665 30.5749 9.71665 28.6429C9.71665 26.7602 8.24769 25.3435 6.30206 25.3435C5.84602 25.3435 5.4106 25.1576 5.08748 24.8258C4.76435 24.494 4.58331 24.0445 4.58331 23.5786L4.5879 17.0152C4.5879 11.6403 8.84581 7.2655 14.08 7.2655H40.92C46.1541 7.2655 50.4144 11.6403 50.4144 17.0152L50.4166 23.3738C50.4166 23.8398 50.2356 24.2916 49.9148 24.6211C49.5916 24.9529 49.1562 25.1388 48.6979 25.1388C46.8164 25.1388 45.2856 26.7108 45.2856 28.6429ZM32.6607 30.1797L35.3626 27.4781C35.8324 27.0121 35.9951 26.325 35.7912 25.6896C35.5895 25.0542 35.0624 24.6024 34.4253 24.5106L30.6922 23.9505L29.0216 20.477C28.7351 19.8793 28.153 19.5075 27.5045 19.5051H27.4999C26.8537 19.5051 26.2716 19.8769 25.9805 20.4747L24.3099 23.9505L20.5837 24.5082C19.9397 24.6024 19.4126 25.0542 19.2087 25.6896C19.007 26.325 19.1697 27.0121 19.6372 27.4781L22.3391 30.1797L21.702 33.9991C21.592 34.658 21.851 35.3122 22.378 35.7052C22.676 35.9241 23.022 36.037 23.3726 36.037C23.6407 36.037 23.9112 35.9688 24.1587 35.8346L27.4999 34.032L30.8343 35.8299C31.4118 36.1476 32.097 36.0982 32.6218 35.7029C33.1512 35.3122 33.4101 34.658 33.3001 33.9991L32.6607 30.1797Z"
-					fill="currentColor"
-				/>
+					fill="currentColor"/>
 			</svg>
 			<h3 class="font-bold text-3xl">Data Strategy</h3>
 			<p class="text-gray-500">
@@ -54,14 +51,12 @@
 				height="51"
 				viewBox="0 0 51 51"
 				fill="none"
-				class="text-indigo-500"
-			>
+				class="text-indigo-500">
 				<path
 					fill-rule="evenodd"
 					clip-rule="evenodd"
 					d="M7.4375 21.6358C7.4375 12.0527 15.6057 4.30713 25.4861 4.30713C35.3943 4.30713 43.5625 12.0527 43.5625 21.6358C43.5625 26.4648 41.7711 30.948 38.8227 34.7479C35.57 38.9395 31.5608 42.5914 27.0482 45.458C26.0153 46.1205 25.0832 46.1705 23.9497 45.458C19.4113 42.5914 15.4022 38.9395 12.1773 34.7479C9.22671 30.948 7.4375 26.4648 7.4375 21.6358ZM19.5377 22.1754C19.5377 25.3857 22.2098 27.9106 25.4861 27.9106C28.7645 27.9106 31.4622 25.3857 31.4622 22.1754C31.4622 18.9901 28.7645 16.3422 25.4861 16.3422C22.2098 16.3422 19.5377 18.9901 19.5377 22.1754Z"
-					fill="currentColor"
-				/>
+					fill="currentColor"/>
 			</svg>
 			<h3 class="font-bold text-3xl">DataOps</h3>
 			<p class="text-gray-500">
@@ -75,8 +70,7 @@
 				height="42"
 				viewBox="0 0 42 42"
 				fill="none"
-				class="text-indigo-500"
-			>
+				class="text-indigo-500">
 				<path
 					d="M20.6679 0.65332C16.1173 14.2562 8.26773 31.1598 0 41.6277L20.6679 33.3768L41.3844 41.6277C33.1182 31.1598 25.263 14.2562 20.6679 0.65332Z"
 					fill="currentColor"
@@ -143,7 +137,7 @@
 				<p class="font-light text-gray-500 mb-6"></p>
 				<button
 					type="button"
-					class="w-full text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-4 text-center mr-3 md:mr-0"
+					class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-4 text-center mr-3 md:mr-0"
 					>Contact Us</button
 				>
 			</form>
